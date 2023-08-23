5153a16065951bfffe50e9bf9023d2ed7e58e193
Mckenzie Irwin <irwinmck@gmail.com>
2023-08-23 19:14:08 -0400

    adding base DB connection logic

--- App.tsx
+++ App.tsx
@@ -1,20 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// React.
+import { useState, useEffect } from 'react';
+import { View } from 'react-native';
+import 'react-native-url-polyfill/auto';
 
+// DB.
+import { supabase } from './lib/subabase';
+import { Session } from '@supabase/supabase-js';
+
+// Pages.
+import Auth from './components/Auth';
+import Account from './components/Account';
+
+/****************************************
+ * - Main App Entry -
+ ***************************************/
 export default function App() {
+  const [session, setSession] = useState<Session | null>(null);
+
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session);
+    });
+
+    supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session);
+    });
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      {session && session.user ? (
+        <Account key={session.user.id} session={session} />
+      ) : (
+        <Auth />
+      )}
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
